14299da08a49cf3b6f8c829b358a20308711c9e2
Sylvia Pump <162073360+sylviapump@users.noreply.github.com>
2024-09-09 16:30:41 +1000

    Update script.js
    fix

--- resources/script.js
+++ resources/script.js
@@ -89,13 +89,29 @@ document.addEventListener('DOMContentLoaded', function () {
         // Collect all the checkbox inputs in the form
         const checkboxes = form.querySelectorAll('input[type="checkbox"]');
 
+        // Define sections to exclude from email
+        const excludeDesigns = [
+            'sampleSetDesign',
+            'saveTheDateDesign',
+            'invitationsDesign',
+            'invitationsRsvpSetDesign',
+            'invitationsRsvpDetailsSetDesign',
+            'programsDesign',
+            'menusDesign',
+            'placeCardsDesign',
+            'favourTagsDesign',
+            'thankYouNoteCardsDesign',
+            'envelopeSealsDesign',
+            'senderAddressLabelsDesign'
+        ];
+
         // Ensure all checkboxes are included in the FormData object with "Yes" for checked and "No" for unchecked
         checkboxes.forEach(checkbox => {
             const designSelect = form.querySelector(`select[name="${checkbox.name}Design"]`);
 
             if (checkbox.checked) {
                 formData.set(checkbox.name, 'Yes');
-                if (designSelect) {
+                if (designSelect && !excludeDesigns.includes(designSelect.name)) {
                     formData.set(designSelect.name, designSelect.value); // Use set instead of append to overwrite value
                 }
             } else {
