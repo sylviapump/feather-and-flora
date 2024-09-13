d3191f7202e7d727f43f865708a4a911485b1759
Sylvia Pump <162073360+sylviapump@users.noreply.github.com>
2024-09-13 10:55:59 +1000

    fixed javascript

--- resources/script.js
+++ resources/script.js
@@ -1,26 +1,37 @@
-document.addEventListener('DOMContentLoaded', function () {
-    // Function to close navbar when a link is clicked
-    function closeNavbar() {
-        const navToggler = document.querySelector('.navbar-toggler');
-        if (navToggler) {
-            const navCollapse = document.querySelector('.navbar-collapse');
-            const icon = navToggler.querySelector('span');
-            if (navCollapse && icon && navCollapse.classList.contains('show')) {
+// Custom JavaScript
+
+// Function to close navbar when link is clicked
+function closeNavbar() {
+    var navToggler = document.querySelector('.navbar-toggler');
+    if (navToggler) {
+        var navCollapse = document.querySelector('.navbar-collapse');
+        var icon = navToggler.querySelector('span');
+        if (navCollapse && icon) {
+            if (navCollapse.classList.contains('show')) {
                 navCollapse.classList.remove('show');
-                icon.classList.toggle('fa-times', false);
-                icon.classList.toggle('navbar-toggler-icon', true);
+                if (icon.classList.contains('fa-times')) {
+                    icon.classList.remove('fa-times');
+                    icon.classList.add('navbar-toggler-icon');
+                }
             }
         }
     }
+}
 
-    // Toggle navbar icon between default and 'X' icon
-    const navbarToggler = document.querySelector('.navbar-toggler');
+// Toggle navbar icon between default and 'X' icon
+document.addEventListener('DOMContentLoaded', function() {
+    var navbarToggler = document.querySelector('.navbar-toggler');
     if (navbarToggler) {
         navbarToggler.addEventListener('click', function () {
-            const icon = navbarToggler.querySelector('span');
+            var icon = navbarToggler.querySelector('span');
             if (icon) {
-                icon.classList.toggle('navbar-toggler-icon');
-                icon.classList.toggle('fa-times');
+                if (icon.classList.contains('navbar-toggler-icon')) {
+                    icon.classList.remove('navbar-toggler-icon');
+                    icon.classList.add('fa', 'fa-times');
+                } else {
+                    icon.classList.remove('fa', 'fa-times');
+                    icon.classList.add('navbar-toggler-icon');
+                }
             }
         });
     }
